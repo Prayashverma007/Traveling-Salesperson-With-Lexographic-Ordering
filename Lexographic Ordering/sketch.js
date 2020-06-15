aab869d25ac412b632b0d339283462625dbe8f87
Prayashverma007 <prayashverma007@gmail.com>
2020-06-15 18:39:49 -0500

    Add files via upload

---
+++ Lexographic Ordering/sketch.js
@@ -0,0 +1,51 @@
+var vals = [0,1,2,3,4,5,6,7];
+
+function setup(){
+    createCanvas(400,300);
+
+}
+
+function draw(){
+    console.log(vals);
+    
+    largestI = -1;
+    for(var i =0; i<vals.length;i++){
+        if(vals[i] < vals[i+1]){
+            largestI = i;
+        }
+        
+    }
+    if (largestI == -1){
+        noLoop();
+        console.log('finish');
+        
+    }
+    
+    largestJ = -1;
+    for(var j =0 ; j < vals.length;j++){
+        if(vals[largestI] < vals[j]){
+            largestJ = j;
+        }
+    }
+    
+    swap(vals,largestI,largestJ);
+    
+    var endArray = vals.splice(largestI+1);
+    endArray.reverse();
+    vals = vals.concat(endArray);
+    
+    background(0);
+    textSize(64);
+    var s = '';
+    for(var i =0 ;i < vals.length;i++){
+        s+=vals[i]
+    }
+    fill(255);
+    text(s,20,height/2);
+}
+
+function swap(a,i,j){
+    var temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
